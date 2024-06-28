7ccae3e66e297339e22b9fe6aaaf9c6fab75ff34
Gnarwhaler <danielnhoward00@gmail.com>
2024-06-27 22:07:44 -0500

    something else

--- Jacks21FA/obj/Debug/net8.0/Jacks21FA.AssemblyInfo.cs
+++ Jacks21FA/obj/Debug/net8.0/Jacks21FA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jacks21FA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9d2ce3743023eb8bd3d4cee5cec1c3686b78e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec4841e379a197f650a976dba0bf054aeba83ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jacks21FA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jacks21FA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
