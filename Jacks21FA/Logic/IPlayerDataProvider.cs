714c4ef58d3fb95d6a1b747d7b94fb05eff7af33
verdicaysen <verdicaysen@gmail.com>
2024-06-30 23:35:53 -0500

    Update IPlayerDataProvider.cs
    Updated comments. Woof.

--- Jacks21FA/Logic/IPlayerDataProvider.cs
+++ Jacks21FA/Logic/IPlayerDataProvider.cs
@@ -1,5 +1,5 @@
 public interface IPlayerDataProvider
 {
-    Player Player { get; } // Provide access to the Player instance
-    PlayerData PlayerData { get; } // Optionally, provide direct access to PlayerData if needed
+    Player Player { get; } // Provide access to the Player instance so that you know, you can inherit crap around everywhere.
+    PlayerData PlayerData { get; } // Optionally, provide direct access to PlayerData if needed, which is a lot. Jeesh.
 }
