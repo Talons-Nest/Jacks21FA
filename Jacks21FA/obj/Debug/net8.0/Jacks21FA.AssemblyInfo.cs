2e08ae4bead5df6901b247db729bf2e460ae19a0
rashley <verdicaysen@gmail.com>
2024-07-06 23:04:11 -0500

    Assembly update.

--- Jacks21FA/obj/Debug/net8.0/Jacks21FA.AssemblyInfo.cs
+++ Jacks21FA/obj/Debug/net8.0/Jacks21FA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jacks21FA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c5491c7636342398b38bd0b071f5cb268a10c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a21d7940c99cada266363ced75dc585bf38ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jacks21FA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jacks21FA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
